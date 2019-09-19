f0039cb355b5be4a7b038bbdb835e79114f2e7e7
rasmvah <rasmvah@github.com>
2019-09-20 00:57:07 +0300

    Before deploying Heroku

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "static", "index.html"));
+})
+
+app.use(express.static('static'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
