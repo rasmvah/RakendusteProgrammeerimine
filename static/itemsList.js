4f5c141677945a9f17533d2d974a668857d415b5
rasmvah <rasmvah@github.com>
2019-10-27 01:40:27 +0300

    Updated files

--- static/itemsList.js
+++ static/itemsList.js
@@ -1,11 +1,5 @@
-//const phones =
-//const laptops =
-
-window.addEventListener("load", () =>{
-    console.log("Hello World!");
-    const app = document.getElementById("item-body");
-    app.append(container);
-});
+const phones = [{}];
+const laptops = [{}];
 
 function createItems(){
     const root = document.getElementById("item-list");
@@ -14,3 +8,32 @@ function createItems(){
         root.append(element);
     })
 }
+
+function createItemElement(item){
+    const anchor = document.createElement("a");
+    anchor.href = `item.html?title=${item.title}&cost=${item.price}&src=${item.imgSrc}`;
+    const itemContainer = document.createElement("div");
+    itemContainer.className = "item";
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imgSrc;
+    const titleElement = document.createElement("div");
+    titleElement.className = "item__title";
+    titleElement.textContent = item.title;
+    const priceElement = document.createElement("div");
+    priceElement.innerText = item.price;
+    priceElement.className = "item__price";
+    
+
+    anchor.append(itemContainer);
+    itemContainer.append(imgElement);
+    itemContainer.append(titleElement);
+    itemContainer.append(priceElement);
+
+    return anchor;
+}
+
+window.addEventListener("load", () =>{
+    console.log("Hello World!");
+    createItems;
+});
+
