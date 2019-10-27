aeca6dbb66b0b4120145b61e2ce205962b828a88
rasmvah <rasmvah@github.com>
2019-10-28 00:46:24 +0200

    Started using React and ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import homepage from "./homepage.js";
-import itempage from "./itempage.js";
+// import homepage from "./homepage.js";
+// import itempage from "./itempage.js";
 
-console.log("index.js file");
+// console.log("index.js file");
 
-window.addEventListener("load", () => {
-    homepage.setup();
-    itempage.setup();
-});
+// window.addEventListener("load", () => {
+//     homepage.setup();
+//     itempage.setup();
+// });
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("app");
+
+console.log('root', root);
+
+ReactDOM.render(
+    React.createElement(
+        "button",
+        {},
+        "I am a button, Hello world!"
+    ),
+    root
+);
